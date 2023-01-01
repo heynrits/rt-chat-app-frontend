8cc9cc38f4083da04f84893fee265010c758a375
John Heinrich Austria <jbaustria@up.edu.ph>
2023-01-01 23:24:54 +0800

    feat: Landing page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import Root from './routes/root'
 import './index.css'
+import Landing from './routes/landing'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: 'welcome',
+        element: <Landing />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/routes/landing.jsx
@@ -0,0 +1,41 @@
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import { Box } from '@mui/system'
+import { Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
+export default function Landing() {
+    const navigate = useNavigate();
+
+    const hasUser = localStorage.getItem('username')
+
+    if (hasUser) {
+        navigate('/chat')
+        return null
+    }
+
+    const handleClickEnter = () => {
+        const username = document.getElementById('username').value
+
+        if (username.length == 0) {
+            alert('Please enter a username!')
+            return
+        }
+
+        localStorage.setItem('username', username);
+        navigate('/chat')
+    }
+
+    return (
+        <Box display="flex" alignItems="center" flexDirection="column" width="100%" sx={{
+            height: '100vh',
+            px: 4,
+            justifyContent: 'center'
+        }}>
+            <Typography variant='h3' pb={10} textAlign="center">Welcome to Chat!</Typography>
+            <Typography variant='body1'>Enter a username:</Typography>
+            <TextField label="@your_username" id="username" variant="standard" fullWidth />
+            <Button variant='contained' fullWidth sx={{ mt: 2, p: 1.5 }} onClick={handleClickEnter}>Enter</Button>
+        </Box>
+    )
+}
