d3377a1681edd167694dab4bd27894a2a95c020f
John Heinrich Austria <jbaustria@up.edu.ph>
2023-01-02 19:47:37 +0800

    update: TextField color and startAdornment

--- src/routes/landing.jsx
+++ src/routes/landing.jsx
@@ -1,7 +1,7 @@
 import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 import { Box } from '@mui/system'
-import { Button } from '@mui/material'
+import { Button, InputAdornment } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 
@@ -37,7 +37,19 @@ export default function Landing() {
         }}>
             <Typography variant='h3' pb={10} textAlign="center">Welcome to Chat!</Typography>
             <Typography variant='body1'>Enter a username:</Typography>
-            <TextField label="@your_username" id="username" variant="standard" fullWidth />
+            <TextField
+                id="username"
+                variant="standard"
+                color="purple"
+                fullWidth
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            @
+                        </InputAdornment>
+                    )
+                }}
+            />
             <Button variant='contained' fullWidth sx={{ mt: 2, p: 1.5 }} onClick={handleClickEnter}>Enter</Button>
         </Box>
     )
